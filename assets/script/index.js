e47828205e797712c054fa393aa1c00f1c9e9f18
Gurveer <gurveer2202@gmail.com>
2022-11-03 11:17:04 -0500

    Add file

---
+++ assets/script/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+
+
+//Add event listener
+function onEvent(event, selector, callback){
+    return selector.addEventListener(event, callback);
+}
+
+//Get HTML element by id
+function getElement(selector, parent = document){
+    return parent.getElementById(selector);
+}
+
+//Select HTML element by selector
+function select(selector, parent = document){
+    return parent.querySelector(selector);
+}
+
+
+
+const form = document.querySelector('form')
+const form1 = select('.form1');
+const form2 = select('.form2');
+const numberOne = select('.calc');
+const numberTwo = select('.output');
+const btn1 = select('.game');
+const output = select('.output .one');
+const output2 = select('.output .two');
+const btn= select('.get-result');
+const first = select('.first');
+const reset = select('.reset');
+const rangeLimit= document.getElementById('rangeLimit');
+const iterations= document.getElementById('iter')
+const msg = select('.msg');
+
+
+
+btn1.addEventListener('click', () =>{
+    numberOne.classList ='block';
+    form1.style.display = 'none';
+   
+    btn1.style.display="none";
+    reset.style.display='block';
+    rangeLimit.innerHTML = range.value;
+
+});
+
+
+reset.addEventListener('click', ()=>{
+    location.reload();
+});
+
+function isNumber(str){
+ let input = str.trim();
+  if(input.length > 0 && !isNaN(input) )
+  return true;
+
+  return false;
+}
+
+
+function isNumber(str){
+ let input = str.trim();
+  if(input.length > 0 && !isNaN(input) )
+  return true;
+
+  return false;
+}
+
+//Adding an event listener
+onEvent('click', btn, function(){
+    let number = Math.floor((Math.random() * 10) +1);  
+ let a = first.value;
+ --iterations.value;
+ if( a == number && isNumber(a) ){
+    output.innerText ='';
+    output2.innerText = `congratulations you won `; 
+    first.value = '';}
+else if ( iterations.value < 1
+    ) {
+    output.innerText = `You are out of chances ` ;
+    first.value ='';
+
+}
+ else{
+    if(number < a &&  a <25 && isNumber(a) ){
+        output.innerText = `Enter lowest number  ${iterations.value} attempt left` ;
+        first.value ='';
+        
+    }else
+    if(number > a && isNumber(a)){
+        output.innerText = `Enter greater number ${iterations.value} attempt left`;
+        first.value ='';
+    }
+    else if( number < a && a > 25 && isNumber(a) ){
+        output.innerText = `Enter less than 25 ${iterations.value} attempt left`;
+        first.value ='';}
+        else{
+    output.innerText = `Enter valid numbers ${iterations.value} attempt left`  ;
+    first.value ='';
+}
+
+}});
+
+
+
+
+
+
